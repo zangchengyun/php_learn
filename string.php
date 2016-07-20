45278b49de52b808e1805de98cf65d8a1fddad6f
zang1985 <573125546@qq.com>
2016-07-20 16:27:25 +0700

    内容追加

--- string.php
+++ string.php
@@ -79,6 +79,68 @@ Email:573125546@qq.com
         //如果$start的值大于字符串的长度，则返回false
         print var_dump(substr('watch out for that tree',30));
         
+        
+        //如果$start + $lenth 大于字符串长度，则返回从$start开始到最后的所有字符
+        
+        print '字符串总长度'.strlen('watch out for that tree');
+        echo "<br><br>";
+        print substr('watch out for that tree',20,30);
+        
+        //字符串总长度23
+        //从第21个字符开始，返回30个，实际上只剩3个
+        //返回  ree
+        
+        
+        
+        echo "<br><br>";
+        //如果$start是负值，substr()会从这个字符串的结尾开始反推
+        //输出 t tree 含空格
+        print substr('watch out for that tree',-6);
+        
+        
+        echo "<br><br>";
+        //如果一个负的 $start值 超过了 这个字符串的起始位置，substr()将$start的值视为0
+        //这时候输出完整的字符串
+        print substr('watch out for that tree',-30);
+        
+        
+        echo "<br><br>";
+        //如果$length是负值，那么substr()会从起始位置反向推算，并截取字符串
+        
+        //从倒数第六个字符，截取3个字符
+        //输出结果 t t
+        print substr('watch out for that tree',-6,3);
+        
+        
+        echo '<hr>';
+        //【替换字符串】
+        //方案：substr_replace()方法
+        
+        //方案1
+        //substr_replace($old_string,$new_substring,$start);
+        //$old_string 要处理的字符串
+        //$new_substring 要替换进去的新的字符串（直到结尾）
+        //$start 替换开始的起始位置（注意0是第一个字符）
+        
+        
+        
+        //从第12个字符，用fish替换掉blue dog的内容
+        print substr_replace('My pet is a blue dog', 'fish', 12);
+        echo "<br><br>";
+        
+        
+        
+        
+        //方案2
+        //substr_replace($old_string,$new_substring,$start,$length);
+        //把从$start开始的$length个字符替换成$new_substring
+        //从第12个字符，用red替换4个字符（4个字符指blue的占位）
+        print substr_replace('My pet is a blue dog', 'red', 12,4);
+        echo "<br><br>";
+        
+        
+        //如果$start是负值
+        
 
         ?>
     </body>
