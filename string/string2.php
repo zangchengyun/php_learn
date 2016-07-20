2fb541e40ce38433a1d3c33ee2fa56b85dc0dcb7
zang1985 <573125546@qq.com>
2016-07-20 17:12:49 +0700

    更改目录，添加新文件

---
+++ string/string2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--
+Author:chengyun
+Email:573125546@qq.com
+Github：https://github.com/zangchengyun/php_learn
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>string练习</title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
